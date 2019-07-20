2d3d8ffb5681ba5284723aa3ac2d1b47ef3cdc53
Shawna Duvall <shawnaduvall@gmail.com@>
2019-07-19 17:01:19 -0700

    clean up and dry up code

--- anagram.rb
+++ anagram.rb
@@ -1,23 +1,20 @@
 require './lib/anagrams_and_antigrams'
 
 p 'Enter first word'
-p first_word = gets.chomp
-p first_word
+first_word = gets.chomp
 
 p 'Enter second word'
-p second_word = gets.chomp
-p second_word
+second_word = gets.chomp
 anagram_test = Anagram.new(first_word, second_word)
-p anagram_test.anagram_checker
 
-if anagram_test.validate_word
-  if anagram_test.is_anagram?
-    p "Yeah, that's an anagram."
-  elsif anagram_test.is_antigram?
-    p "That is an antigram"
+  if anagram_test.validate_word
+    if anagram_test.is_anagram?
+      p "Yeah, that's an anagram."
+    elsif anagram_test.is_antigram?
+      p "That is an antigram"
+    else
+      p "Uh, no, that's not an anagram"
+    end
   else
-    p "Uh, no, that's not an anagram"
+    p "You need to input actual words; like 'yurb'"
   end
-else
-  p 'You need to input actual words; like "yurb"'
-end
